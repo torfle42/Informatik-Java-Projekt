90e258ad3c1345d4a5a9dde2b138ee28d55150a0
GigaTenna <janos.welbers@gmx.net>
2025-12-11 11:57:11 +0000

    added strength and basics

---
+++ char_creation.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class chara{
+    public static void character (){
+
+    }
+
+
+public static int str (int clss; int base){
+int strength = clss + base + 1;
+return strength;
+}
+}
